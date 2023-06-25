674d50a21b85c8448eb4fee9008532cf7e47d65b
Yuhki <yuhkixx@protonmail.com>
2023-06-25 21:47:46 +0200

    moved items from dashboard to aimbot tab for direct access

--- imgui menu/tabs/aimbot/aimbot.cpp
+++ imgui menu/tabs/aimbot/aimbot.cpp
@@ -4,5 +4,28 @@
 
 void aimbot::renderaimbot()
 {
-    
+    ImGui::SetCursorPos(ImVec2(75, 10));
+    if (c::tabbutton("SubTab1"))
+    {
+        ImGui::SetCursorPos(ImVec2(65, 50));
+        c::groupbox("GROUPBOX1", ImVec2(285, 225));
+        {
+            ImGui::PushFont(util::segoeuiSemibold);
+            c::button("Button");
+            c::checkbox("Checkbox", &util::checkTest);
+            ImGui::PopFont();
+        }
+        c::endgroupbox();
+        ImGui::SetCursorPos(ImVec2(359, 50));
+        c::groupbox("GROUPBOX2", ImVec2(285, 225));
+        {
+
+        }
+        c::endgroupbox();
+    }
+    ImGui::SetCursorPos(ImVec2(140, 10));
+    if (c::tabbutton("SubTab2"))
+    {
+
+    }
 }
--- imgui menu/tabs/dashboard/dashboard.cpp
+++ imgui menu/tabs/dashboard/dashboard.cpp
@@ -4,28 +4,5 @@
 
 void dashboard::renderdashboard()
 {
-    ImGui::SetCursorPos(ImVec2(75, 10));
-    if (c::tabbutton("Helpers", ImVec2(55, 25)))
-    {
-        ImGui::SetCursorPos(ImVec2(65, 50));
-        c::groupbox("GENERAL", ImVec2(285, 225));
-        {
-            ImGui::PushFont(util::segoeuiSemibold);
-            c::button("Button");
-            c::checkbox("Checkbox", &util::checkTest);
-            ImGui::PopFont();
-        }
-        c::endgroupbox();
-        ImGui::SetCursorPos(ImVec2(359, 50));
-        c::groupbox("INDICATORS", ImVec2(285, 225));
-        {
-
-        }
-        c::endgroupbox();
-    }
-    ImGui::SetCursorPos(ImVec2(140, 10));
-    if (c::tabbutton("Movement", ImVec2(80, 25)))
-    {
-
-    }
+    
 }
